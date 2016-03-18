5404e7a6eac60309cd634f705e9105ba2b968f2f
Patrick Snape <patricksnape@gmail.com>
2016-03-18 18:14:12 +0000

    Make sure that the correct Python lib is chosen
    Unix only - Jenkins was failing (OSX)

--- conda/build.sh
+++ conda/build.sh
@@ -4,6 +4,19 @@ cd build
 
 export LDFLAGS="-L${PREFIX}/lib $LDFLAGS"
 
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  DYNAMIC_EXT="so"
+fi
+if [ "$(uname -s)" == "Darwin" ]; then
+  DYNAMIC_EXT="dylib"
+fi
+
+if [ $PY3K -eq 1 ]; then
+  export PY_STR="${PY_VER}m"
+else
+  export PY_STR="${PY_VER}"
+fi
+
 cmake -LAH .. \
 -DCMAKE_INSTALL_PREFIX=$PREFIX \
 -DCMAKE_BUILD_TYPE=Release \
@@ -16,6 +29,7 @@ cmake -LAH .. \
 -DBUILD_TESTING=0 \
 -DBUILD_DOCUMENTATION=0 \
 -DBUILD_SHARED_LIBS=1 \
+-DPYTHON_LIBRARY="$PREFIX/lib/libpython$PY_STR.$DYNAMIC_EXT" \
 -DPYTHON_EXECUTABLE=$PYTHON \
 -DVTK_WRAP_PYTHON=1 \
 -DVTK_INSTALL_PYTHON_MODULE_DIR=$SP_DIR
