b8fb8bcb2eafc4cb98041bec319c26cbea038a14
Toni Gurgui <toni.gurgui@gmail.com>
2018-10-11 08:41:40 +0200

    * Added openvr

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+git clone https://github.com/ValveSoftware/openvr.git
+export OPENVR_INCLUDE_DIR=./openvr/include
+export OPENVR_LIBRARY_TEMP=./openvr/lib/osx32/libopenvr_api.dylib
 mkdir build
 cd build
 
@@ -17,6 +21,8 @@ else
   export PY_STR="${PY_VER}"
 fi
 
+
+
 cmake -LAH .. \
 -DCMAKE_INSTALL_PREFIX=$PREFIX \
 -DCMAKE_BUILD_TYPE=Release \
@@ -28,10 +34,12 @@ cmake -LAH .. \
 -DBUILD_EXAMPLES=0 \
 -DBUILD_TESTING=0 \
 -DBUILD_DOCUMENTATION=0 \
--DBUILD_SHARED_LIBS=1 \
 -DPYTHON_LIBRARY="$PREFIX/lib/libpython$PY_STR.$DYNAMIC_EXT" \
 -DPYTHON_EXECUTABLE=$PYTHON \
 -DVTK_WRAP_PYTHON=1 \
--DVTK_INSTALL_PYTHON_MODULE_DIR=$SP_DIR
+-DVTK_INSTALL_PYTHON_MODULE_DIR=$SP_DIR \
+-DModule_VTKRenderingOpenVR=1 \
+-DOPENVR_INCLUDE_DIR=$OPENVR_INCLUDE_DIR\
+-DOPENVR_LIBRARY_TEMP=$OPENVR_LIBRARY_TEMP 
 
 make -j$CPU_COUNT && make install
